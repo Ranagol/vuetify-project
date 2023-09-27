8d83bb26005df6d6ce6e5cab3a355e9a11d1a048
Ranagol <andorhorvat@gmail.com>
2023-09-27 19:04:58 +0200

    CSS issues corrected.

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app>
+    <!-- App bar -->
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn to="/" rounded>Home</v-btn>
+      <v-btn to="/login" rounded>Login</v-btn>
+    </v-app-bar>
+
+    <!-- ROUTER -->
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+
+    <!-- Footer -->
+    <div>
+      <br>
+      <v-footer class="bg-grey-lighten-1">
+        <v-row justify="center" no-gutters>
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            color="white"
+            variant="text"
+            class="mx-2"
+            rounded="xl"
+          >
+            {{ link }}
+          </v-btn>
+          <v-col class="text-center mt-1" cols="12">
+            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
+    </div>
+
+
+
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Login from '@/views/Login.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      showPassword: false,
+      links: [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us',
+      ],
+    }
+  },
+});
+
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+    <!-- Login component -->
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+
+          <!-- USERNAME -->
+          <v-text-field
+            label="Username"
+            prepend-icon="mdi-account-circle"
+          />
+
+          <!-- PASSWORD -->
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'md-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          />
+          <!-- @click:append = this means: if the user clicks on the appended icon, then... -->
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+
+        <!-- BUTTONS -->
+        <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Login</v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LoginPage',
+  components: {},
+  data() {
+    return {
+      showPassword: false,
+    }
+  },
+});
+
+</script>
